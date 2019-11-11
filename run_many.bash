f6555528300672ceb07018a63deaefa887f01bb6
Robert Williamson <williamson.robert.j+science@gmail.com>
2019-11-11 16:17:20 -0500

    bash script now runs 50 replicates of each input set, results also uploaded

--- run_many.bash
+++ run_many.bash
@@ -1,4 +1,5 @@
 
+COUNT=50
 h=(0 0.5 1)
 n_male=50
 n_female=50
@@ -6,10 +7,14 @@ p1=0.05
 p2=0.95
 n_offspring=10
 
-for d in "${h[@]}"
+counter=1
+while [ $counter -le $COUNT ]
 do
+  for d in "${h[@]}"
+  do
 
-  python sim.py -M $n_male -F $n_female -p $p1 $p2 -d $d -o $n_offspring
+    python sim.py -M $n_male -F $n_female -p $p1 $p2 -d $d -o $n_offspring
 
+  done
+  ((counter++))
 done
-
